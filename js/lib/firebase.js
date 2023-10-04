88bf16d90a11490c96593993b6e6214f05557854
Nijin Nazar <nijin@qburst.com>
2023-10-04 11:39:55 +0530

    Add: Firebase config

---
+++ js/lib/firebase.js
@@ -0,0 +1,27 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.4.0/firebase-app.js";
+import {
+  getDatabase,
+  ref,
+  onValue,
+} from "https://www.gstatic.com/firebasejs/10.4.0/firebase-database.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCVJaFfoEBM3-ZqT89iRzQ-1K9dKIoThO8",
+  authDomain: "hrm-app-6cb10.firebaseapp.com",
+  databaseURL:
+    "https://hrm-app-6cb10-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "hrm-app-6cb10",
+  storageBucket: "hrm-app-6cb10.appspot.com",
+  messagingSenderId: "588841693832",
+  appId: "1:588841693832:web:7a98a8759551243ee0653d",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getDatabase(app);
+
+// const starCountRef = ref(db, "/");
+// onValue(starCountRef, (snapshot) => {
+//   const data = snapshot.val();
+//   console.log(data);
+// });
