9dd0f23dde9da5ffb508b35b98e1980c7dddc1c6
nijin-26 <nijin@qburst.com>
2023-10-08 20:53:49 +0530

    Add: Complete functionality of Toast

--- js/utils/toast.js
+++ js/utils/toast.js
@@ -1,7 +1,42 @@
-export const showToast = (type, msg, payloadObj) => {
-  if (type === "warning") {
-  } else if (type === "error") {
-    console.log(msg, payloadObj);
-  } else if (type === "success") {
-  } else return;
+const toastContainer = document.querySelector(".toast-container");
+
+const icons = {
+  success: "new_releases",
+  error: "report",
+  warning: "warning",
 };
+
+const defaultMsgs = {
+  success: "Successful",
+  error: "Error Occured. Try Again.",
+  warning: "Warning. Please check again.",
+};
+
+let timeOutId_1;
+let timeOutId_2;
+
+export const showToast = (type, msg, payloadObj) => {
+  toastContainer.innerHTML += `  
+   <div class="${type} active">
+        <span class="material-symbols-outlined toast-icon">${
+          icons[type]
+        } </span>
+        <p>${msg ? msg : defaultMsgs[type]}</p>
+    </div>`;
+
+  timeOutId_1 = setTimeout(() => {
+    toastContainer.querySelector(`.${type}`).classList.remove("active");
+  }, 3000);
+
+  timeOutId_2 = setTimeout(() => {
+    toastContainer.querySelector(`.${type}`).remove();
+  }, 6000);
+};
+
+// showToast("warning");
+
+window.addEventListener("unload", () => {
+  console.log("unload");
+  clearTimeout(timeOutId_1);
+  clearTimeout(timeOutId_2);
+});
