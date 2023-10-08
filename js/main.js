38edba1ccf82c70f9e7c018df117c13bbb937b31
nijin-26 <nijin@qburst.com>
2023-10-08 16:20:46 +0530

    Refactor: Main.js - user actions and event handling

--- js/main.js
+++ js/main.js
@@ -17,6 +17,7 @@ const roleFilterInput = document.querySelector(".role-filter");
 const filterContainer = document.querySelector(".filter-container");
 const searchSkillInput = document.querySelector(".skill-search-input");
 const skillList = document.querySelector(".dropdown-content");
+const selectedSkillsContainer = document.querySelector(".selected-skill");
 
 // Buttons
 const openFilterBtn = document.querySelector(".enable-filter-btn");
@@ -26,6 +27,33 @@ const addEmployeeBtn = document.querySelector(".add-employee-btn");
 export let selectedSkillsArray = [];
 export let employeeData = [];
 
+const setSelectedSkills = (selectedSkills) => {
+  selectedSkillsArray = selectedSkills;
+};
+
+const resetSkillFilter = () => {
+  searchSkillInput.value = "";
+  searchSkillInput.dispatchEvent(new Event("input"));
+  document
+    .querySelectorAll(".selected-skill > p")
+    .forEach((tagEl) => tagEl.remove());
+  selectedSkillsArray = [];
+  filterTable();
+};
+
+// <<<< Employee Table Events >>>>>>>>>>
+const handleEmployeeTableClick = (e) => {
+  if (e.target.id === "edit-action-btn") {
+    console.log("Edit btn clicked", e.target.dataset);
+  } else if (e.target.id === "delete-action-btn") {
+    console.log("Delete btn is clicked");
+  } else if (e.target.tagName === "TH" && e.target.dataset.column) {
+    sortTable(e.target.dataset.column);
+  } else {
+    console.log(e.target.closest("tr").dataset);
+  }
+};
+
 document.addEventListener("DOMContentLoaded", () => {
   fetchSkills((skills) => renderSkills(skills));
 
@@ -34,16 +62,9 @@ document.addEventListener("DOMContentLoaded", () => {
     renderTable(employeeArr);
   });
 
-  searchEmployeeInput.addEventListener("input", () => {
-    filterTable();
-  });
-
+  searchEmployeeInput.addEventListener("input", () => filterTable());
   departmentFilterInput.addEventListener("input", filterTable);
   roleFilterInput.addEventListener("input", filterTable);
-
-  const setSelectedSkills = (selectedSkills) => {
-    selectedSkillsArray = selectedSkills;
-  };
 
   openFilterBtn.addEventListener("click", () =>
     filterContainer.classList.toggle("open-filter-options")
@@ -75,72 +96,49 @@ document.addEventListener("DOMContentLoaded", () => {
     filterTable();
   });
 
-  const resetSkillFilter = () => {
-    searchSkillInput.value = "";
-    searchSkillInput.dispatchEvent(new Event("input"));
-    document
-      .querySelectorAll(".selected-skill > p")
-      .forEach((tagEl) => tagEl.remove());
-    selectedSkillsArray = [];
-    filterTable();
-  };
-
   addEmployeeBtn.addEventListener("click", () => {
     console.log("Add employee btn clicked");
   });
 
-  document
-    .querySelector(".selected-skill")
-    .addEventListener("click", (e) =>
-      removeSelectedSkills(e, setSelectedSkills)
-    );
+  selectedSkillsContainer.addEventListener("click", (e) =>
+    removeSelectedSkills(e, setSelectedSkills)
+  );
 
-  // <<<< Employee Table Events >>>>>>>>>>
-  employeeTable.addEventListener("click", (e) => {
-    if (e.target.id === "edit-action-btn") {
-      console.log("Edit btn clicked", e.target.dataset);
-    } else if (e.target.id === "delete-action-btn") {
-      console.log("Delte btn is clicked");
-    } else if (e.target.tagName === "TH") {
-      if (e.target.dataset.column) sortTable(e.target.dataset.column);
+  employeeTable.addEventListener("click", handleEmployeeTableClick);
+
+  // <<<<<< Keyboard shortcuts for Skill Search Input >>>>>>>>
+  searchSkillInput.addEventListener("keydown", (event) => {
+    if (event.key === "Enter") {
+      skillList.style.display = "none";
+      renderSelectedSkills(event, setSelectedSkills);
+      return;
+    } else if (event.key === "Backspace" && event.target.value === "") {
+      if (selectedSkillsArray.length !== 0) {
+        removeSelectedSkills(event, setSelectedSkills);
+      }
     } else {
-      console.log(e.target.closest("tr").dataset);
-    }
-
-    // <<<<<< Keyboard shortcuts for Skill Search Input >>>>>>>>
-    searchSkillInput.addEventListener("keydown", (event) => {
-      if (event.key === "Enter") {
-        skillList.style.display = "none";
-        renderSelectedSkills(event, setSelectedSkills);
-        return;
-      } else if (event.key === "Backspace" && event.target.value === "") {
-        if (selectedSkillsArray.length !== 0) {
-          removeSelectedSkills(event, setSelectedSkills);
-        }
-      } else {
-        skillList.style.display = "block";
-      }
-    });
-  });
-
-  // <<<<<< Keyboard Shortcuts / Events for Document - START  >>>>>>>>>>>>>
-
-  document.addEventListener("keydown", (e) => {
-    if (e.key === "/" && document.activeElement !== searchSkillInput) {
-      e.preventDefault();
-      searchEmployeeInput.focus();
-    } else if (e.key === "Escape") {
-      if (document.activeElement === searchEmployeeInput) {
-        searchEmployeeInput.blur();
-      } else if (document.activeElement === searchSkillInput) {
-        searchSkillInput.blur();
-      }
-    } else if ((e.ctrlKey || e.metaKey) && e.shiftKey) {
-      if (e.key === "e" || e.key === "E") toggleTheme();
-      else if (e.key === "f" || e.key === "F")
-        filterContainer.classList.toggle("open-filter-options");
+      skillList.style.display = "block";
     }
   });
-
-  // <<<<<< Keyboard Shortcuts / Events for Document - END  >>>>>>>>>>>>>
 });
+
+// <<<<<< Keyboard Shortcuts / Events for Document - START  >>>>>>>>>>>>>
+
+document.addEventListener("keydown", (e) => {
+  if (e.key === "/" && document.activeElement !== searchSkillInput) {
+    e.preventDefault();
+    searchEmployeeInput.focus();
+  } else if (e.key === "Escape") {
+    if (document.activeElement === searchEmployeeInput) {
+      searchEmployeeInput.blur();
+    } else if (document.activeElement === searchSkillInput) {
+      searchSkillInput.blur();
+    }
+  } else if ((e.ctrlKey || e.metaKey) && e.shiftKey) {
+    if (e.key === "e" || e.key === "E") toggleTheme();
+    else if (e.key === "f" || e.key === "F")
+      filterContainer.classList.toggle("open-filter-options");
+  }
+});
+
+// <<<<<< Keyboard Shortcuts / Events for Document - END  >>>>>>>>>>>>>
